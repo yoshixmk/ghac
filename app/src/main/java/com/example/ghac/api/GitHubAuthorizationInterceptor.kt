e9906df65a92ba715ccb3433b0f715d9c1905139
yoshixmk <pirori67@gmail.com>
2023-03-23 10:15:49 +0900

    use hilt for retrofit and okhttp. chores: gradle files

---
+++ app/src/main/java/com/example/ghac/api/GitHubAuthorizationInterceptor.kt
@@ -0,0 +1,26 @@
+package com.example.ghac
+
+import com.example.ghac.api.Constants
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class GitHubAuthorizationInterceptor : Interceptor {
+
+    var token: String = Constants.GITHUB_PAT
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var request = chain.request()
+
+        if (request.header("No-Authentication") == null) {
+            if (!token.isEmpty()) {
+                val finalToken = "Bearer $token"
+                request = request.newBuilder()
+                    .addHeader("Authorization", finalToken)
+                    .build()
+            }
+
+        }
+
+        return chain.proceed(request)
+    }
+}
