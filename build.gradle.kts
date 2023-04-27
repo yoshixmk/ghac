8af8a920eb99e0c4f1aa7c6371a8438a4e39894c
yoshixmk <pirori67@gmail.com>
2023-04-27 20:52:59 +0900

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 buildscript {
-    val composeUiVersion by extra("1.4.1")
+    val composeUiVersion by extra("1.4.2")
     val pagingVersion by extra("3.1.1")
-    val kotestVersion by extra("5.6.0")
+    val kotestVersion by extra("5.6.1")
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
